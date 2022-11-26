323c8621f10a9f4b47cdaccf438be3b49b8314f7
Duc Anh Le (FG-232) <Duc-Anh.Le@bmw.de>
2022-11-26 22:53:51 +0100

    Add day 7 part 1

---
+++ src/main/kotlin/Day7.kt
@@ -0,0 +1,44 @@
+import utils.Solver
+import kotlin.math.abs
+
+class Day7 : Solver(7) {
+    override fun solvePart1(input: String): Any {
+        val crabPositions = input.split(",").map { it.toInt() }
+
+        var fuel = 0
+
+        val highestOccurrences = crabPositions.groupingBy { it }.eachCount().toList().sortedBy { (key, value) -> value }.reversed()
+        val highestOccurrence = highestOccurrences[0].first
+
+        for (pos in crabPositions) {
+            fuel += abs(pos - highestOccurrence)
+        }
+
+        for (spot in highestOccurrences) {
+            val currentSpot = spot.first
+            var currentFuel = 0
+
+            for (pos in crabPositions) {
+                currentFuel += abs(pos - currentSpot)
+            }
+
+            if (currentFuel < fuel) {
+                fuel = currentFuel
+            }
+        }
+
+        return fuel
+    }
+
+    override fun solvePart2(input: String): Any {
+        return 0
+    }
+
+}
+
+fun main() {
+    val testData = "16,1,2,0,4,2,7,1,2,14"
+
+    Day7().verifyAndSolve(testData, 37, "#1")
+    //Day7().verify(testData, 37, "#1")
+}
